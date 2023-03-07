099afa4f3655c6a917152b6eb41162fe97e57ac7
Wessel <w.roovers1@student.avans.nl>
2023-03-07 12:50:18 +0100

    ha

--- Week2/003.Colors/src/Colors.java
+++ Week2/003.Colors/src/Colors.java
@@ -33,6 +33,51 @@ public class Colors extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        int j = 0;
+        for (int i = 0; i < 650 ; i=i+50) {
+            Area rectangle = new Area(new Rectangle(i, 0, 50, 50));
+            if(j==0){
+                graphics.setColor(Color.BLACK);
+            }
+            else if(j==1){
+                graphics.setColor(Color.BLUE);
+            }
+            else if(j==2){
+                graphics.setColor(Color.CYAN);
+            }
+            else if(j==3){
+                graphics.setColor(Color.darkGray);
+            }
+            else if(j==4){
+                graphics.setColor(Color.green);
+            }
+            else if(j==5){
+                graphics.setColor(Color.lightGray);
+            }
+            else if(j==6){
+                graphics.setColor(Color.magenta);
+            }
+            else if(j==7){
+                graphics.setColor(Color.orange);
+            }
+            else if(j==8){
+                graphics.setColor(Color.pink);
+            }
+            else if(j==9){
+                graphics.setColor(Color.RED);
+            }
+            else if(j==10){
+                graphics.setColor(Color.white);
+            }
+            else if(j==11){
+                graphics.setColor(Color.yellow);
+            }
+            else if(j==12){
+                graphics.setColor(Color.gray);
+            }
+            graphics.fill(rectangle);
+            j++;
+        }
     }
 
 
