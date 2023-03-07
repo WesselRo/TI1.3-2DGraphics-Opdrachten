be7f95f8675d8d6df4193592bf8cf9f779d67765
Wessel <w.roovers1@student.avans.nl>
2023-03-07 13:52:33 +0100

    ha

--- Week2/004.GradientPaint/src/GradientPaintExercise.java
+++ Week2/004.GradientPaint/src/GradientPaintExercise.java
@@ -13,19 +13,22 @@ import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
 
 public class GradientPaintExercise extends Application {
-    private ResizableCanvas canvas;
+    private BorderPane mainPane = new BorderPane();
+    private ResizableCanvas canvas = new ResizableCanvas(g -> draw(g), mainPane);
 
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        BorderPane mainPane = new BorderPane();
-        canvas = new ResizableCanvas(g -> draw(g), mainPane);
+
+
         mainPane.setCenter(canvas);
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.setTitle("GradientPaint");
         primaryStage.show();
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
+
     }
+
 
 
     public void draw(FXGraphics2D graphics)
@@ -33,9 +36,28 @@ public class GradientPaintExercise extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
-        Area area = new Area(new Rectangle((int) canvas.getWidth(), (int) canvas.getHeight()));
-        graphics.setPaint(new RadialGradientPaint());
-        graphics.fill(area);
+
+        float radius1 = 100;
+        float[] dist1 = {0.0f, 0.2f, 1.0f};
+        Point2D center1 = new Point2D.Float((int) canvas.getWidth()/2, (int) canvas.getHeight()/2);
+        Color[] colors1 = {Color.YELLOW, Color.green, Color.PINK};
+        RadialGradientPaint radialGradientPaint1 = new RadialGradientPaint(center1, radius1, dist1, colors1, MultipleGradientPaint.CycleMethod.REFLECT);
+        graphics.setPaint(radialGradientPaint1);
+        Area area1 = new Area(new Rectangle((int) canvas.getWidth(), (int) canvas.getHeight()));
+        graphics.fill(area1);
+        canvas.setOnMouseDragged(e ->
+        {
+            float radius = 100;
+            float[] dist = {0.0f, 0.2f, 1.0f};
+            Point2D center = new Point2D.Float((float)e.getX(),(float) e.getY());
+            Color[] colors = {Color.YELLOW, Color.green, Color.PINK};
+            RadialGradientPaint radialGradientPaint = new RadialGradientPaint(center, radius, dist, colors, MultipleGradientPaint.CycleMethod.REFLECT);
+            graphics.setPaint(radialGradientPaint);
+            Area area = new Area(new Rectangle((int) canvas.getWidth(), (int) canvas.getHeight()));
+            graphics.fill(area);
+        });
+
+
     }
 
 
