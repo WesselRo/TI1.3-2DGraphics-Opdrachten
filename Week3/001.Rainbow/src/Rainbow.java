0b247d79215a27114480ec9d38c14b53c8be04ba
Wessel <w.roovers1@student.avans.nl>
2023-03-08 11:56:32 +0100

    ha

--- Week3/001.Rainbow/src/Rainbow.java
+++ Week3/001.Rainbow/src/Rainbow.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.ArrayList;
 
 import javafx.application.Application;
 
@@ -28,17 +29,53 @@ public class Rainbow extends Application {
     }
 
 
+
     public void draw(FXGraphics2D graphics)
     {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        graphics.translate((int) canvas.getWidth()/2, (int) canvas.getHeight()/2);
+        //Font font = new Font("Serif", Font.PLAIN, 50);
+//        AffineTransform tx = new AffineTransform();
+//        tx.rotate(-.5*Math.PI);
+       double centerX = canvas.getWidth()/2;
+       double centerY = canvas.getHeight()/2;
+
+        String woord = "Regenboog";
+        double radius = Math.min(centerX, centerY) - 45;
+        double angle = -90.0;
+        double increase = 180 / woord.length();
+
+        Font font = new Font("Dialog", Font.PLAIN, 100);
+        graphics.setFont(font);
+
+        for (int i = 0; i < woord.length(); i++) {
+            char letterChar = woord.charAt(i);
+            graphics.setColor(getRainbowColor(i, woord.length()));
+            AffineTransform tx;
+            tx = AffineTransform.getTranslateInstance(centerX, centerY+100);
+            tx.rotate(Math.toRadians(angle + i * increase));
+            tx.translate(0, -radius);
+            graphics.setTransform(tx);
+
+            graphics.drawString(Character.toString(letterChar), 0, 0);
+        }
+    }
+
+    private Color getRainbowColor(int index, int total) {
+        float kaas = (float) index / (float) total;
+        return Color.getHSBColor(kaas, 1, 1);
     }
 
 
     public static void main(String[] args)
     {
         launch(Rainbow.class);
+    }
+
+    public void getWoord(String woord){
+
     }
 
 }
